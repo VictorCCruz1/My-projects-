df8928654927e124259f181d6f1ad9f5b2ca0fac
Victor Cruz <93475981+VictorCCruz1@users.noreply.github.com>
2023-03-17 08:25:25 -0300

    Add files via upload

---
+++ --- Aprendizegem ---.py
@@ -0,0 +1,7 @@
+aprendizagem = '''=> Fundamentos da linguagens
+=> Comandos básicos
+=> Operadores aritméticos lógicos
+=> Operadores aritméticos relacionais
+=> Utilização de módulos
+=> Estruturas relacionais básicas'''
+print(aprendizagem)

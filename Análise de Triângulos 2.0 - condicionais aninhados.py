1b145a4bbf461905473a2ed817ede3e43e96b92a
Victor Cruz <93475981+VictorCCruz1@users.noreply.github.com>
2023-03-21 20:21:47 -0300

    Add files via upload

--- Análise de Triângulos 2.0 - condicionais aninhados.py
+++ Análise de Triângulos 2.0 - condicionais aninhados.py
@@ -10,13 +10,15 @@ if (lados[0] + lados[1]) < lados[2]:
     mensagem = ('Os lados {} e {} juntos são menores que {}, portanto não é possível '
                 'formar um triângulo.'.format(lados[0],lados[1],lados[2]))
 
-elif lados[0] == lados[1] == lados[2]:
-    mensagem = ('O triângulo de lados {} é equilátero.'.format(lados[0]))
+else:   
+    
+    if lados[0] == lados[1] == lados[2]:
+        mensagem = ('O triângulo de lados {} é equilátero.'.format(lados[0]))
 
-elif lados[0] == lados[1] or lados[1] == lados[2]:
-    mensagem = ('O triângulo de lados {}, {} e {} é isósceles.'.format(lados[0],lados[1],lados[2]))
+    elif lados[0] == lados[1] or lados[1] == lados[2]:
+        mensagem = ('O triângulo de lados {}, {} e {} é isósceles.'.format(lados[0],lados[1],lados[2]))
 
-elif lados[0] != lados[1] and lados[1] != lados[2]:
-    mensagem = (f'O triângulo de lados {lados[0]}, {lados[1]} e {lados[2]} é escaleno.')
+    elif lados[0] != lados[1] and lados[1] != lados[2]:
+        mensagem = (f'O triângulo de lados {lados[0]}, {lados[1]} e {lados[2]} é escaleno.')
 
 print(mensagem)

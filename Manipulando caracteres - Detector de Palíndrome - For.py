3e351bd8ffbabcc1d07b8a18923945c585797418
Victor Cruz <93475981+VictorCCruz1@users.noreply.github.com>
2023-03-24 20:47:55 -0300

    Add files via upload

---
+++ Manipulando caracteres - Detector de Palíndrome - For.py
@@ -0,0 +1,13 @@
+print('=-'*17+'\nDETECTOR DE PALÍNDROME\n'+'=-'*17)
+
+frase = str(input('Digite a frase: ')).strip().upper()
+palavra = ''.join(frase.split())
+inverso = ''
+for i in range (len(palavra) -1, -1, -1):
+    inverso += palavra[i]
+    if inverso == palavra:
+        mensagem = (f'Temos um palíndrome, {inverso}!')
+    else:
+        mensagem = (f'Não temos um palíndrome, a palavra é {palavra} e o inverso é {inverso}!')
+
+print(mensagem)

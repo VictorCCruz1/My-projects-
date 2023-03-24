d300eb593404f3da227825da5809b68c0a4aa970
Victor Cruz <93475981+VictorCCruz1@users.noreply.github.com>
2023-03-24 20:50:41 -0300

    Add files via upload

--- Manipulando Cadeia de Caracteres - Métodos.py
+++ Manipulando Cadeia de Caracteres - Métodos.py
@@ -1,24 +1,25 @@
-
-frase = 'oi Victor, tudo bem?'
-len(frase)  # comprimento da frase
-frase.count('o')  # quantas vezes tem a legra o em frase
-frase.count('o', 0, 13)  # a contagem vai do primeiro ao
-# décimo segundo caracter da string.
-frase.find('tudo')  # vai identificar pela posição da primeira
-# letra de 'tudo'
-frase.find('tesouro')  # retorna valor -1, não há essa string
-'tesouro' in frase  # retorna true
-frase.replace('oi', 'olá')  # substitui em frase 'oi' por 'olá'
-frase.upper()  # torna maiúsculo
-frase.lower()  # torna tudo maiúsculo
-frase.capitalize()  # [0] fica maiúsculo e o resto todo minúsaculo
-frase.title()  # as primeiras letras das palavras em maiúsculo
-frase.strip()  # remove espaço vazios antes e depois das letras
-frase.rstrip()  # método strip pela direita, no fim da string
-frase.lstrip()  # método strup pela esquerda, so no início da string
-frase.split()  # cada espaço vazio na cadeia de caracteres é removido
-# criando uma lista com todas as palavras separadas.
-print('-'.join(frase))  # pega as palavras splitadas e junta-as com '-'
-format((a)[2:]) ('''singifica que vai do 3° 
-# caracter até o último da string da 
-# variável a.''')
+#
+#frase = 'oi Victor, tudo bem?'
+#len(frase)  # comprimento da frase
+#frase.count('o')  # quantas vezes tem a legra o em frase
+#frase.count('o', 0, 13)  # a contagem vai do primeiro ao
+## décimo segundo caracter da string.
+#frase.find('tudo')  # vai identificar pela posição da primeira
+## letra de 'tudo'
+#frase.find('tesouro')  # retorna valor -1, não há essa string
+#'tesouro' in frase  # retorna true
+#frase.replace('oi', 'olá')  # substitui em frase 'oi' por 'olá'
+#frase.upper()  # torna maiúsculo
+#frase.lower()  # torna tudo maiúsculo
+#frase.capitalize()  # [0] fica maiúsculo e o resto todo minúsaculo
+#frase.title()  # as primeiras letras das palavras em maiúsculo
+#frase.strip()  # remove espaço vazios antes e depois das letras
+#frase.rstrip()  # método strip pela direita, no fim da string
+#frase.lstrip()  # método strip pela esquerda, so no início da string
+#frase.split()  # cada espaço vazio na cadeia de caracteres é removido
+## criando uma lista com todas as palavras separadas.
+#print('-'.join(frase))  # pega as palavras splitadas e junta-as com '-'
+#format((a)[2:]) ('''singifica que vai do 3° 
+## caracter até o último da string da 
+## variável a.''')
+#print(f'{lista[x]}') #retonar o item da lista da posição do valor de x 

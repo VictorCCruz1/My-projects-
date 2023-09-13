976a8ba3fecb231a8cec5aea0f4a49b4b08d2438
Victor Cruz <93475981+VictorCCruz1@users.noreply.github.com>
2023-09-13 20:19:49 -0300

    Add files via upload

---
+++ Printf e Tipos Básicos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int inteiro = 5;
+    float fraçao = 1.55;
+    char caracter = 'S';
+    char texto[] = "texto";
+
+    printf("Inteiro: %i.\n", inteiro);
+    printf("Fracionário: %f. \n", fraçao);
+    printf("Caracter: %d. \n", caracter);
+    printf("Texto: %s. \n", texto);
+    
+    return 0;
+}
